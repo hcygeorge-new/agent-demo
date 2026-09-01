5e6d0ea58f28a5dd0bc14bd7cd92bcc39293efbc
george.huang (黃慶昀) <george.huang@gigabyte.com>
2026-09-01 09:37:41 +0800

    test copliot

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('hello george')
+def add(a, b):
+    return a + b
+
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+    print("All tests passed")
+
+
+if __name__ == "__main__":
+    test_add()
